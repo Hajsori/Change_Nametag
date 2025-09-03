866bf38af0d90b1475031108f96ccc9e42840e3d
Pludam <109984063+Pludam@users.noreply.github.com>
2025-09-03 20:29:03 +0200

    Update parse_manifest.py

--- .github/workflows/scripts/parse_manifest.py
+++ .github/workflows/scripts/parse_manifest.py
@@ -9,4 +9,4 @@ version_array = [str(x) for x in manifest["header"]["min_engine_version"]]
 version = version.join(version_array)
 
 
-print(f"::set-output name=MC-Version::{version}")
+print(f"::set-output name=mc_version::{version}")

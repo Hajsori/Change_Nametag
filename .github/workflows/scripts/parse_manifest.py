1c36f4f966e1df7dff0f2675154e7be6cc8efc34
Pludam <109984063+Pludam@users.noreply.github.com>
2025-09-03 20:18:12 +0200

    Create parse_manifest.py

---
+++ .github/workflows/scripts/parse_manifest.py
@@ -0,0 +1,12 @@
+import json
+
+with open("manifest.json") as f:
+    manifest = json.load(f)
+
+version = "."
+
+version_array = [str(x) for x in manifest["header"]["min_engine_version"]]
+version = version.join(version_array)
+
+
+print(f"::set-output name=MC-Version::{version}")
